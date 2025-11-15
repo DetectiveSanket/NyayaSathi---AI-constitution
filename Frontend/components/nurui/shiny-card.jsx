b2cba83c5a9f10e25009ebf81105b88291c7dd0d
Sanketniza <141250234+Sanketniza@users.noreply.github.com>
2025-11-16 00:40:07 +0530

    feat: Update ShinyCard styles with new primary color and gradient adjustments

--- Frontend/components/nurui/shiny-card.jsx
+++ Frontend/components/nurui/shiny-card.jsx
@@ -1,7 +1,7 @@
 import "./styles/shiny-card.css";
 const ShinyCard = ({ featureName, featureDescription, icon }) => {
   return (
-    <div className="shiny-card flex flex-col items-center justify-center min-h-72 shadow-2xl dark:shadow-none border border-[#00ccff] rounded-2xl p-6 transition-transform duration-300 hover:scale-105">
+    <div className="shiny-card flex flex-col items-center justify-center min-h-72 shadow-2xl dark:shadow-none border border-[#00ccff] rounded-2xl p-6 transition-transform duration-00 hover:scale-105">
       <div className="flex items-center justify-center mb-4 w-14 h-14 rounded-xl group hover:scale-125 hover:rotate-12 transition-all duration-500">
         {icon}
       </div>
